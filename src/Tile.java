5c836b6864dc6608f151c33ca93e504e405507a1
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-02-21 20:52:02 +0100

    Setting an regullar minefield, generated randomly in a random direction.

--- src/Tile.java
+++ src/Tile.java
@@ -1,47 +1,44 @@
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
 
 import java.io.File;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class Tile extends Rectangle {
+    private static final Random rn = new Random();
 
     private boolean hasBomb = false;
     // boolean clickable stands for making the rest of application stucked when explodes.
     private boolean clickable = true;
+    private int tileId;
 
-    Random rn = new Random();
+    private static final AtomicInteger count = new AtomicInteger(0);
 
-    public Tile (int x, int y, Pane root){
+
+    public Tile (int x, int y){
+
         setWidth(SaperMain.TILE_SIZE);
         setHeight(SaperMain.TILE_SIZE);
         relocate(SaperMain.TILE_SIZE * x, SaperMain.TILE_SIZE * y);
         setFill(Color.ORANGE);
         setStroke(Color.RED);
         setStrokeWidth(0.5);
-        if (x == rn.nextInt(SaperMain.BOARD_SIZE - 1) && y == rn.nextInt(SaperMain.BOARD_SIZE - 1)) {
-            this.hasBomb = true;
-        }
         this.setOnMouseClicked(e -> {
             if (clickable) {
                 if (!hasBomb) {
                     this.setFill(Color.BEIGE);
-                    // deleting the commentary slashes below will make the game playable and put the bombs in a few places.
                 } else {
                     jihad();
                     this.setFill(Color.RED);
                 }
             }
         });
+        tileId = count.incrementAndGet();
     }
     private void jihad () {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -51,5 +48,12 @@ public class Tile extends Rectangle {
         String uriString = new File("allahu-akbar-sound-effect-download-link-BMA0XO7C.mp3").toURI().toString();
         MediaPlayer player = new MediaPlayer( new Media(uriString));
         player.play();
+    }
+    public int getTileId(){
+        return tileId;
+    }
+
+    public void setHasBomb(boolean hasBomb) {
+        this.hasBomb = hasBomb;
     }
 }
--- src/TileFactory.java
+++ src/TileFactory.java
@@ -1,8 +1,15 @@
 import javafx.scene.layout.Pane;
 
+import java.util.Random;
+
 public class TileFactory {
+    private static final Random rn = new Random();
+    private int randomInt = rn.nextInt(4) + 2;
     public Tile getInstance(int x, int y, Pane pane){
-        Tile tile = new Tile(x,y, pane);
+        Tile tile = new Tile(x,y);
+        if (tile.getTileId() % randomInt  == 0){
+            tile.setHasBomb(true);
+        }
         return tile;
     }
 }
