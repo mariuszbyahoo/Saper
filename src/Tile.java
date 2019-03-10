535a63d97a9b013a7623562193adabb53ce70e1a
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-03-10 09:39:06 +0100

    Creating an exit() method which exits the app when exploded. When Thread.sleep(x) mehod called, then MediaPlayer stops playing. Need to fix it.

--- src/Tile.java
+++ src/Tile.java
@@ -5,15 +5,12 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 import java.io.File;
-import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class Tile extends Rectangle {
-    private static final Random rn = new Random();
 
     private boolean hasBomb = false;
-    // boolean clickable stands for making the rest of application stucked when explodes.
     private boolean clickable = true;
     private int tileId;
 
@@ -33,8 +30,10 @@ public class Tile extends Rectangle {
                 if (!hasBomb) {
                     this.setFill(Color.BEIGE);
                 } else {
-                    jihad();
                     this.setFill(Color.RED);
+                    jihad();
+                    exit();
+
                 }
             }
         });
@@ -48,6 +47,8 @@ public class Tile extends Rectangle {
         String uriString = new File("allahu-akbar-sound-effect-download-link-BMA0XO7C.mp3").toURI().toString();
         MediaPlayer player = new MediaPlayer( new Media(uriString));
         player.play();
+        clickable = false;
+
     }
     public int getTileId(){
         return tileId;
@@ -55,5 +56,15 @@ public class Tile extends Rectangle {
 
     public void setHasBomb(boolean hasBomb) {
         this.hasBomb = hasBomb;
+    }
+    public void exit() {
+        if (!clickable) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.exit(1);
+        }
     }
 }
